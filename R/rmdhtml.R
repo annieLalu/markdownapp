39f91386ef5fdad08ce5855fe3749254dc26aae5
root <root@localhost>
2017-08-07 14:05:50 +0200

    commit

--- R/rmdhtml.R
+++ R/rmdhtml.R
@@ -1,10 +1,10 @@
 ## This app requires OpenCPU 1.0.1 or higher !!!! 
 ##
-library(knitr)
+##library(knitr)
 #' @export
 rmdhtml<- function(text){
 if(text == "index"){
-  knit2html("www/index.Rmd", output="output.html");
+  knit2html("../www/index.Rmd", output="output.html");
  }
  invisible();
 }
