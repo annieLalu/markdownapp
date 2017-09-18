9745c87c1055bee6f8bc4e4bd062a3bfd9702ad7
root <root@localhost>
2017-09-18 15:39:56 +0200

    final url system

--- R/rmdhtml.R
+++ R/rmdhtml.R
@@ -1,10 +1,9 @@
 ## This app requires OpenCPU 1.0.1 or higher !!!! 
 ##
-##library(knitr)
 #' @export
 rmdhtml<- function(){
     url <- system.file("/www/index.Rmd")
-    knit2html(url, output="output.html");
- invisible();
+    knit2html(url, output="output.html")
+ invisible()
 }
 
