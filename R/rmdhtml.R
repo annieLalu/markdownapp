8eafc3cd2ff50165042a22e8bacbd0d407b9d92e
root <root@localhost>
2017-09-18 15:45:14 +0200

    final url system

--- R/rmdhtml.R
+++ R/rmdhtml.R
@@ -2,7 +2,7 @@
 ##
 #' @export
 rmdhtml<- function(){
-    url <- system.file("/www/index.Rmd")
+    url <- system.file("/www/index.Rmd", package = "markdownapp")
     knit2html(url, output="output.html")
  invisible()
 }
